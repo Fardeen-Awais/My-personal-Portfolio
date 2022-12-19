5ddb510933ec7d856821647de07db6c9518a3130
Fardeen Awais <fardeenjutt99@gmail.com>
2022-12-19 09:31:01 -0800

    Transfering my project

--- fardeen-portfolio-backend/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'o6h5kv64',
-    dataset: 'production'
-  }
-})
--- fardeen-portfolio-backend/schemas/index.js
+++
@@ -1,6 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import author from './author'
-import ProfileInfo from './ProfileInfo'
-
-export const schemaTypes = [ProfileInfo, author, category, blockContent]
